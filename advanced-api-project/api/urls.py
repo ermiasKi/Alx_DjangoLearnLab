d9d54f85e3f0fe298a3a27d4eeab7f60607ba789
ermias <ermiaskindalem97@gmail.com>
2025-09-28 18:00:39 +0300

    advanced api

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,9 +3,9 @@ from .views import ListView, DeleteView, DetailView, CreateView,UpdateView
 
 
 urlpatterns = [
-    path('book/', ListView.as_view(), name='book-list'),
-    path('book/detail/<int:id>', DetailView.as_view(), name='book-detail'),
-    path('book/create', CreateView.as_view(), name='book-create'),
-    path('book/update/<int:id>', UpdateView.as_view(), name='=book-update'),
-    path('book/delete/<int:id>', DeleteView.as_view(), name='book-delete'),
+    path('books/', ListView.as_view(), name='book-list'),
+    path('books/detail/<int:id>', DetailView.as_view(), name='book-detail'),
+    path('books/create', CreateView.as_view(), name='book-create'),
+    path('books/update/<int:id>', UpdateView.as_view(), name='=book-update'),
+    path('books/delete/<int:id>', DeleteView.as_view(), name='book-delete'),
 ]
