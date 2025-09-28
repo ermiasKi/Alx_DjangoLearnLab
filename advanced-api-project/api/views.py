6fa835a6aeb6ae5b1e3e4d73fd4b22a467c96c11
ermias <ermiaskindalem97@gmail.com>
2025-09-28 18:01:51 +0300

    advanced api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import action
 from rest_framework import generics
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 from django_filters import rest_framework
